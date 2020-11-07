277540fb2666808d0a7324496a7e3240a32d282f
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-11-07 18:19:09 +0530

    Base Spring Project

---
+++ src/main/java/com/ritchieeinstein/stockproject/ticksimulator/TicksimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.stockproject.ticksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicksimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicksimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ritchieeinstein/stockproject/ticksimulator/TicksimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.stockproject.ticksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicksimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
