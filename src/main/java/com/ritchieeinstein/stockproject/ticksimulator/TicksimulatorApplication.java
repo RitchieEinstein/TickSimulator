f5842b64212b86c74566baa0b3c9c1abff0a1046
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-11-08 00:00:11 +0530

    Bootloader now starts the generator

--- src/main/java/com/ritchieeinstein/stockproject/ticksimulator/TicksimulatorApplication.java
+++ src/main/java/com/ritchieeinstein/stockproject/ticksimulator/TicksimulatorApplication.java
@@ -1,13 +1,20 @@
 package com.ritchieeinstein.stockproject.ticksimulator;
 
+import com.ritchieeinstein.stockproject.ticksimulator.service.TickGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class TicksimulatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicksimulatorApplication.class, args);
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		ApplicationContext context = SpringApplication.run(TicksimulatorApplication.class, args);
+		TickGenerator generator = (TickGenerator)context.getBean(TickGenerator.class);
+		generator.run();
 	}
 
 }
